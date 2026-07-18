1da8ee5f218234cabbff414e7458d1afc91c442f
김지윤 <b01090443093@gmail.com>
2026-07-18 20:28:43 +0900

    [Chore] CI/CD 설정

---
+++ src/main/java/backend/agerdon/AgerdonApplication.java
@@ -0,0 +1,13 @@
+package backend.agerdon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgerdonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgerdonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/agerdon/AgerdonApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.agerdon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgerdonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
