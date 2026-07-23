6f927b9bde0e02700d9fb391901daa0c523ce4d4
원지현 <gtgsky2022@gmail.com>
2026-07-23 13:26:52 +0900

    [Feat] PlaceRepository 구현 및 검색 쿼리 메서드 추가

--- src/main/java/backend/agerdon/domain/place/entity/Place.java
+++ src/main/java/backend/agerdon/domain/place/entity/Place.java
@@ -1,4 +1,43 @@
 package backend.agerdon.domain.place.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "place")
 public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "place_id")
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String addr;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private PlaceType type;
+
+    @Builder
+    public Place(String name, String addr, PlaceType type) {
+        this.name = name;
+        this.addr = addr;
+        this.type = type;
+    }
+
+    // 정보(장소 이름, 주소, 구분) 수정용 메서드
+    public void update(String name, String addr, PlaceType type) {
+        this.name = name;
+        this.addr = addr;
+        this.type = type;
+    }
 }
--- src/main/java/backend/agerdon/domain/place/entity/PlaceType.java
+++ src/main/java/backend/agerdon/domain/place/entity/PlaceType.java
@@ -1,4 +1,7 @@
 package backend.agerdon.domain.place.entity;
 
 public enum PlaceType {
+
+    ORIGIN, // 출발지 (건물, 학교 내 위치 등)
+    DEST    // 목적지 (지하철역, 버스 정류장 등)
 }
